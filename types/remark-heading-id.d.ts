5e246515b94611973e3e2124f12a13f8f9fa9b70
Johan Schuijt <johan@resolve.works>
2025-12-10 20:39:14 +0100

    pre-commit hook

--- types/remark-heading-id.d.ts
+++ types/remark-heading-id.d.ts
@@ -1,10 +1,10 @@
-import type { Plugin } from 'unified'
-
-type Options = {
-  defaults: boolean
-}
-
 declare module 'remark-heading-id' {
-  const remarkHeadingId: Plugin<[(Options | undefined)?]>
-  export default remarkHeadingId
+  import type { Plugin } from 'unified';
+
+  type Options = {
+    defaults: boolean;
+  };
+
+  const remarkHeadingId: Plugin<[(Options | undefined)?]>;
+  export default remarkHeadingId;
 }
