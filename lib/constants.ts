864cc4f5d57645daf11a9a8701c4ac3a341001d5
Johan Schuijt <johan@resolve.works>
2026-01-27 17:34:16 +0100

    remove unused constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,26 +1,13 @@
-export const SITE = 'EveryPolitician';
 export const BASE_URL = 'https://www.everypolitician.org';
 export const OSA_URL = 'https://www.opensanctions.org';
 export const LICENSE_URL = 'https://creativecommons.org/licenses/by-nc/4.0/';
 export const CMS_URL = 'https://opensanctions.directus.app';
 export const MAIN_DATASET = 'default';
 export const EMAIL = 'info@opensanctions.org';
-export const FEATURED_COLLECTIONS = ['default', 'sanctions'];
-export const EXTRA_COLLECTIONS = [
-  'peps',
-  'securities',
-  'maritime',
-  'kyb',
-  'openownership',
-  'crime',
-];
 export const GA_TRACKING_ID = 'G-KGY1GYY598';
 export const REVALIDATE_BASE = 3600 * 2;
 export const REVALIDATE_LONG = REVALIDATE_BASE * 2;
 export const THEME_COLOR = '#2563eb';
-export const SOCIAL_IMAGE_URL =
-  'https://assets.opensanctions.org/images/nura/social.png';
-export const RSS_MIME = 'application/rss+xml';
 
 export const API_TOKEN = process.env.API_TOKEN;
 
